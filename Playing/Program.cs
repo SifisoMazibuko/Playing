f5e8b19954e9bec780ebb28a1175e60c3a069b4a
Sifiso Samson Mazibuko <SifisoMazibuko@hotmail.com>
2017-09-19 09:24:00 +0200

    progress

--- Playing/Program.cs
+++ Playing/Program.cs
@@ -6,7 +6,27 @@ namespace Playing
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Func<int, bool> CheckForPrimeNumber = p =>
+            {
+                bool isPrimeNumber = false;
+                int i;
+                for (i = 2; i <= p - 1; i++)
+                {
+                    if (p % i == 0) {
+                        isPrimeNumber = false;
+                        break;
+                    }
+                }
+                if (i == p) {
+                    isPrimeNumber = true;
+                }
+                return isPrimeNumber;
+            };
+            Console.WriteLine("Enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            bool isPrime = CheckForPrimeNumber(number);
+            Console.WriteLine($"{number} is {(isPrime ? "" : "not")} prime number");
             Console.Read();
         }
     }
